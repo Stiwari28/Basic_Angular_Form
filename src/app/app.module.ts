df1b04c92e221141532fb720c47ea4e51832ef5f
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-06-22 14:10:58 +0530

    New Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormItemsComponent } from './form-items/form-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-items/form-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-items',
+  templateUrl: './form-items.component.html',
+  styleUrls: ['./form-items.component.css']
+})
+export class FormItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
